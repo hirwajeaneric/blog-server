be16278b98f6aa5fa0f3364e033a793333d4d1d4
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-11 21:06:42 +0200

    Automated test added

--- test/test.js
+++ test/test.js
@@ -16,18 +16,109 @@ describe("Blogs", () => {
     describe("/GET blog", () => {
         it("it should GET all the blogs", (done) => {
             chai
-            .request(app)
-            .get("/api/blogs")
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.data.should.be.a("array");
-                res.body.data.length.should.be.eql(0);
-                done();
-            });
+                .request(app)
+                .get("/api/blogs")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.a("array");
+                    res.body.data.length.should.be.eql(0);
+                    done();
+                });
         });
     });
 
     describe("/POST blog", () => {
-        
+        it("it should add blog", (done) => {
+            let blog = {
+                title: "This is a test blog",
+                body: "This is the body of our test blog",
+                image: "http://image.unsplash.com/photo-123243"
+            };
+            chai
+                .request(app)
+                .post("/api/blogs")
+                .send(blog)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("title");
+                    res.body.should.have.property("body");
+                    res.body.should.have.property("image");
+                    res.body.should.have.property("_id");
+                    done();
+                })
+        });
     })
+
+    describe("/GET/:id blog", () => {
+        it("it should GET a blog by the id", (done) => {
+            let blog = new Blog({
+                title: "This is a test blog",
+                body: "This is the body of our test blog",
+                image: "http://image.unsplash.com/photo-123243"
+            });
+
+            blog.save((err, blog) => {
+                chai
+                  .request(app)
+                  .get("/api/blogs/"+blog.id)
+                  .send(blog)
+                  .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a("object");
+                        res.body.status.should.be.eql("success");
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe("/PUT/:id blog", () => {
+        it("it should PUT a blog by the id", (done) => {
+            let blog = new Blog({
+                title: "This is a test blog",
+                body: "This is the body of our test blog",
+                image: "http://image.unsplash.com/photo-123243"
+            });
+            blog.save((err, blog) => {
+                console.log(blog.id)
+                chai
+                    .request(app)
+                    .put("/api/blogs/"+blog.id)
+                    .send({
+                        title: "This is a test blog",
+                        body: "This is the body of our test blog",
+                        image: "http://image.unsplash.com/photo-123243"
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a("object");
+                        res.body.status.should.be.eql("success");
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe("/DELETE/:id blog", () => {
+        it("it should DELETE a blog by the id", (done) => {
+            let blog = new Blog({
+                title: "This is a test blog",
+                body: "This is the body of our test blog",
+                image: "http://image.unsplash.com/photo-123243"
+            });
+            blog.save((err, blog) => {
+                chai
+                    .request(app)
+                    .delete("/api/blogs/"+blog.id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a("object");
+                        res.body.status.should.be.eql("success");
+                        done();
+                    });
+            });
+        });
+    });
 });
+
